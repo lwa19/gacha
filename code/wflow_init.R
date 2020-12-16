dc3df7de1f8b0af09f7a3b11ba098a0faac882a9
Lijia Wang <lwa19@users.noreply.github.com>
2020-12-16 17:24:26 -0600

    added bibliography doc

--- code/wflow_init.R
+++ code/wflow_init.R
@@ -1,10 +1,16 @@
 library("workflowr")
 wflow_git_config(user.name = "Leah Wang", user.email = "lwang19@uchicago.edu")
 
+# Initializing repo
 setwd("~/Documents/GitHub/gacha")
 wflow_start("gacha", git = F)
 wflow_build()
 wflow_status()
 wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd"),
               "Publish the initial files for myproject")
-wflow_use_github("lwa19")
+wflow_use_github("lwa19") # how come I always forget this useful function. 
+
+
+# New pages: 
+wflow_open('analysis/index.Rmd')
+wflow_publish("analysis/index.Rmd", "intro")
