3513e72c93c5b1c10c9923b2efd0069d06e8afb4
Lijia Wang <lwa19@users.noreply.github.com>
2020-12-18 23:21:09 -0600

    .

--- code/wflow_init.R
+++ code/wflow_init.R
@@ -13,7 +13,7 @@ wflow_use_github("lwa19") # how come I always forget this useful function.
 
 # New pages: 
 wflow_open('analysis/pity.Rmd')
-wflow_publish("analysis/pity.Rmd", "started pity file")
+wflow_publish("analysis/pity.Rmd", "fixed equations")
 wflow_publish(c("analysis/index.Rmd", "analysis/gacha.Rmd", "analysis/bonus.Rmd", 
                 "analysis/pity.Rmd"), 
               "case studies AFTER thesis")
